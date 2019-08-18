7c96af0425a321f0bef94bd5a38685c439c4e882
Vladimir Netyosa <v.netyosa@Vladimirs-MacBook-Pro.local>
2019-08-19 01:14:22 +0300

    fixed init bank

--- src/controllers/Bank.ts
+++ src/controllers/Bank.ts
@@ -13,7 +13,7 @@ export class Bank {
   }
 
   initialize() {
-    let value = this.getFromLocalStorage();
+    let value = this.getFromLocalStorage() || {};
     this.amount = value.amount || 100;
     this.bet = value.bet || 5;
     this.createUI();
